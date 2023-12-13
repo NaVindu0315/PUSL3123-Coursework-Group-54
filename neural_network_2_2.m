ea1c8d9921fc540c5287f1ef6cce1ddb3cd12cde
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-13 17:44:55 +0530

    ss

--- neural_network_2_2.m
+++ neural_network_2_2.m
@@ -6,11 +6,6 @@ load fisheriris;
 new_data = meas;
 label_data = grp2idx(species);
 
-%to get the no of rows 
-rows_count = size(meas,1);
-%combining 
-
 %to shuffle
-rng("shuffle");
-sfld_meas= meas(randperm(size(meas,1)),:);
-sfld_species= species(randperm(size(species,1)),:);
+rng(1); 
+i = randperm(size(new_data));
