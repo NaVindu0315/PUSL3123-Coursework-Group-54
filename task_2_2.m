de81ecbb97bae1ce404cb7c512ac6c599a090bf8
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-13 18:39:01 +0530

    ss

--- task_2_2.m
+++ task_2_2.m
@@ -11,6 +11,9 @@ trn_p = 0.6;
 %splitting dataset to test and train
 training_count =floor(trn_p*size(new_meas,1));
 
+%data
 trainging_data = new_meas(1:training_count,:);
 testing_data = new_meas(training_count+1:end,:);
 
+%target
+training_target = new_species(1:training_count);
