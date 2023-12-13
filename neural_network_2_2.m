cf353514d8c6323c880bb3f9126baade6ce6a768
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-13 17:54:13 +0530

    ss

--- neural_network_2_2.m
+++ neural_network_2_2.m
@@ -8,7 +8,7 @@ label_data = grp2idx(species);
 
 %to shuffle
 rng(1); 
-num_samples = size(new_data, 1);
-i=rander
+count = size(new_data, 1);
+i=randperm(count);
 new_data = new_data(i,:);
 label_data = label_data(i);
