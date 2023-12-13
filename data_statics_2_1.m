6115c2d971379826728700bb122c294ecec83363
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-13 10:33:07 +0530

    removed species

--- data_statics_2_1.m
+++ data_statics_2_1.m
@@ -14,7 +14,7 @@ disp(["No of Rows in species : ",num2str(N2)]);
 % to get the featurs from 1-4 
 
 %creating the list for attritbutes
-atr = {"sepal length","sepal width","petal length","petal width","species"}
+atr = {"sepal length","sepal width","petal length","petal width"}
 
 for x =1:4
     %assigning values
