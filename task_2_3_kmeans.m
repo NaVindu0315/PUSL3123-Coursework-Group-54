768310f39ec2cd34d9658537d5925c12aad0ecf9
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-14 10:40:01 +0530

    s

--- task_2_3_kmeans.m
+++ task_2_3_kmeans.m
@@ -13,4 +13,6 @@ evl_mtrcs = zeros(length(k_values),1);
 for i =1:length(k_values)
     %k means clustering for every k value
     k2 = k_values(i);
-    [indx,c] = kmeans(X)
+    [indx,c] = kmeans(new_data,k2);
+    %evaluating the clutering perfomance
+    
