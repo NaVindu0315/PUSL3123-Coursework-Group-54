8cd3af2f0e4a627f839bfff9912ddace9ee00a7a
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-14 20:02:27 +0530

    working

--- task_2_3_kmeans.m
+++ task_2_3_kmeans.m
@@ -85,8 +85,8 @@ for i =1:length(k_values)
   hold on;
   plot(centroids(:, 1), centroids(:, 2), 'kx', 'MarkerSize', 15, 'LineWidth', 3);xlabel('Feature 1');
   ylabel('Feature 2');
-    legend('Cluster 1', 'Cluster 2', 'Cluster 3', 'Centroids');
-    hold off;
+  legend('Cluster 1', 'Cluster 2', 'Cluster 3', 'Centroids');
+  hold off;
         
    
 end
