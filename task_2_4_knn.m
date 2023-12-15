fe999b90df2a2361f07284fde364bc6b0d439d3f
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-15 12:13:25 +0530

    chnged variables

--- task_2_4_knn.m
+++ task_2_4_knn.m
@@ -1,19 +1,19 @@
 %loading fisherirs
-load fisherirs;
+load fisheriris;
 % Shuffling the dataset
 rng("default");
 indx = randperm(size(meas,1));
-new_meas = meas(indx,:);
+meas = meas(indx,:);
 new_species = species(indx);
 % Training percentage
 trn_p = 0.6;
 
 % Spliting dataset into test and train
-training_count = floor(trn_p * size(new_meas,1));
+training_count = floor(trn_p * size(meas,1));
 
 % Data
-training_data = new_meas(1:training_count,:);
-testing_data = new_meas(training_count+1:end,:);
+training_data = meas(1:training_count,:);
+testing_data = meas(training_count+1:end,:);
 
 % Target
 training_target = new_species(1:training_count);
