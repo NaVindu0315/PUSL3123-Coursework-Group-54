8b9312b4cb2b54a26d895268a897bd4410a10af6
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-15 12:07:08 +0530

    Update task_2_4_knn.m

--- task_2_4_knn.m
+++ task_2_4_knn.m
@@ -7,3 +7,6 @@ new_meas = meas(indx,:);
 new_species = species(indx);
 % Training percentage
 trn_p = 0.6;
+
+% Spliting dataset into test and train
+training_count = floor(trn_p * size(new_meas,1));
