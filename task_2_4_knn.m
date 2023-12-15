560b3c899d5a204d8fd3d0cb707f11fbd8d0b8f3
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-15 13:01:39 +0530

    sad

--- task_2_4_knn.m
+++ task_2_4_knn.m
@@ -1,44 +1,48 @@
 %loading fisherirs
 load fisheriris;
 %q1
-% Shuffling the dataset
+% % Shuffling the dataset
+% rng("default");
+% indx = randperm(size(meas,1));
+% meas = meas(indx,:);
+% species = species(indx);
+% % Training percentage
+% trn_p = 0.6;
+% 
+% % Spliting dataset into test and train
+% training_count = floor(trn_p * size(meas,1));
+% 
+% % Data
+% training_data = meas(1:training_count,:);
+% testing_data = meas(training_count+1:end,:);
+% 
+% % Target
+% training_target = species(1:training_count);
+% testing_target = species(training_count+1:end);
+% 
+% 
+% % Converting species labels to numerical values for the neural network
+% species_labels = unique(species);
+% num_species = length(species_labels);
+% 
+% new_training_targets = zeros(num_species, length(training_target));
+% 
+% for i = 1:length(training_target)
+%     species_index = find(strcmp(species_labels, training_target{i}));
+%     new_training_targets(species_index, i) = 1;
+% end
+% 
+% %for testing target
+% 
+% new_testing_targets =  zeros(num_species,length(testing_target));
+% for i = 1:length(testing_target)
+%     species_index = find(strcmp(species_labels, testing_target{i}));
+%     new_testing_targets(species_index, i) = 1;
+% end
+
+%shuffleing the dataset 
 rng("default");
-indx = randperm(size(meas,1));
-meas = meas(indx,:);
-species = species(indx);
-% Training percentage
-trn_p = 0.6;
-
-% Spliting dataset into test and train
-training_count = floor(trn_p * size(meas,1));
-
-% Data
-training_data = meas(1:training_count,:);
-testing_data = meas(training_count+1:end,:);
-
-% Target
-training_target = species(1:training_count);
-testing_target = species(training_count+1:end);
-
-
-% Converting species labels to numerical values for the neural network
-species_labels = unique(species);
-num_species = length(species_labels);
-
-new_training_targets = zeros(num_species, length(training_target));
-
-for i = 1:length(training_target)
-    species_index = find(strcmp(species_labels, training_target{i}));
-    new_training_targets(species_index, i) = 1;
-end
-
-%for testing target
-
-new_testing_targets =  zeros(num_species,length(testing_target));
-for i = 1:length(testing_target)
-    species_index = find(strcmp(species_labels, testing_target{i}));
-    new_testing_targets(species_index, i) = 1;
-end
+shfld_indc = randperm
 %q3
 %defining k values for evaluating
 k_values = [5,7];
