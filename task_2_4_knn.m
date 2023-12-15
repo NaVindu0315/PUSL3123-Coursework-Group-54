dccd055ae16a2b53889ceca517cbb3784a45664a
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-12-15 13:37:31 +0530

    q3 working

--- task_2_4_knn.m
+++ task_2_4_knn.m
@@ -1,44 +1,7 @@
 %loading fisherirs
 load fisheriris;
 %q1
-% % Shuffling the dataset
-% rng("default");
-% indx = randperm(size(meas,1));
-% meas = meas(indx,:);
-% species = species(indx);
-% % Training percentage
-% trn_p = 0.6;
-% 
-% % Spliting dataset into test and train
-% training_count = floor(trn_p * size(meas,1));
-% 
-% % Data
-% training_data = meas(1:training_count,:);
-% testing_data = meas(training_count+1:end,:);
-% 
-% % Target
-% training_target = species(1:training_count);
-% testing_target = species(training_count+1:end);
-% 
-% 
-% % Converting species labels to numerical values for the neural network
-% species_labels = unique(species);
-% num_species = length(species_labels);
-% 
-% new_training_targets = zeros(num_species, length(training_target));
-% 
-% for i = 1:length(training_target)
-%     species_index = find(strcmp(species_labels, training_target{i}));
-%     new_training_targets(species_index, i) = 1;
-% end
-% 
-% %for testing target
-% 
-% new_testing_targets =  zeros(num_species,length(testing_target));
-% for i = 1:length(testing_target)
-%     species_index = find(strcmp(species_labels, testing_target{i}));
-%     new_testing_targets(species_index, i) = 1;
-% end
+
 
 %shuffleing the dataset 
 rng("default");
@@ -48,15 +11,15 @@ trn_p =0.6;
 tst_p =0.4;
 
 total_records = numel(shfld_indc);
-no_train_records = floor(trn_p*total_records);
-no_test_records = total_records - no_train_records;
+no_trainining_records = floor(trn_p*total_records);
+no_testing_records = total_records - no_trainining_records;
 
 %spltting dta
-train_data = meas(shfld_indc(1:no_train_records),:);
-test_data =meas(shfld_indc(no_train_records+1:end),:);
+training_data = meas(shfld_indc(1:no_trainining_records),:);
+testing_data =meas(shfld_indc(no_trainining_records+1:end),:);
 
-training_target = species(shfld_indc(1:no_train_records));
-testing_target = species(shfld_indc(no_train_records+1:end));
+training_target = species(shfld_indc(1:no_trainining_records));
+testing_target = species(shfld_indc(no_trainining_records+1:end));
 
 
 
